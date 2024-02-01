9745b7f5acc1fc455eb66ff0912c7cca1fe47e3e
LesterLin <newa5812763@gmail.com>
2024-02-01 22:18:53 +0800

    大事件實作-更新文章分類和添加文章分類-分組校驗

--- src/main/java/com/lesterlin/bigevent/pojo/Category.java
+++ src/main/java/com/lesterlin/bigevent/pojo/Category.java
@@ -3,13 +3,14 @@ package com.lesterlin.bigevent.pojo;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.groups.Default;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
 public class Category {
-    @NotNull
+    @NotNull(groups = Update.class)
     private Integer id; //主鍵ID
     @NotEmpty
     private String categoryName; //分類名稱
@@ -20,4 +21,15 @@ public class Category {
     private LocalDateTime createTime; //創建時間
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime updateTime; //更新時間
+
+    // 如果某個校驗項沒有指定分組，默認屬於default分組
+    // 分組間可以繼承，A extends B 那麼A中擁有B中所有的校驗項
+
+    public interface Add extends Default {
+
+    }
+
+    public interface Update extends Default{
+
+    }
 }
