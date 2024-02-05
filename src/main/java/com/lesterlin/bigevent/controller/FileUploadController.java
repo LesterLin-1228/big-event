d9dd62f6189300c92407198eaba9efde089ea8ab
LesterLin <newa5812763@gmail.com>
2024-02-05 17:53:31 +0800

    大事件實作-文件上傳阿里雲OSS

--- src/main/java/com/lesterlin/bigevent/controller/FileUploadController.java
+++ src/main/java/com/lesterlin/bigevent/controller/FileUploadController.java
@@ -1,6 +1,7 @@
 package com.lesterlin.bigevent.controller;
 
 import com.lesterlin.bigevent.pojo.Result;
+import com.lesterlin.bigevent.utils.AliOssUtil;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,12 +14,14 @@ import java.util.UUID;
 public class FileUploadController {
 
     @PostMapping("/upload")
-    public Result<String> upload(MultipartFile file) throws IOException {
+    public Result<String> upload(MultipartFile file) throws Exception {
         // 把文件的內容儲存到本地磁碟
         String originalFilename = file.getOriginalFilename();
-        // 保證文件名唯一，防止文件覆蓋
+        // 透過UUID保證文件名唯一，防止文件覆蓋
         String filename = UUID.randomUUID().toString()+originalFilename.substring(originalFilename.lastIndexOf("."));
-        file.transferTo(new File("C:\\Users\\user\\Desktop\\files\\"+filename));
-        return Result.success("url訪問地址...");
+//        file.transferTo(new File("C:\\Users\\user\\Desktop\\files\\"+filename));
+        // 把文件內容儲存到阿里雲上
+        String url = AliOssUtil.uploadFile(filename,file.getInputStream());
+        return Result.success(url);
     }
 }
